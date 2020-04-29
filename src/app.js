7d8cdd06277a6562f13e09f6cc84b470d9bb0c44
Yongqiang Cao <yongqiang.cao@shopee.com>
2020-04-29 23:06:56 +0800

    Remove useless code

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
-const MongoClient = require('mongodb').MongoClient;
-const mongo = require('koa-mongo');
 const route = require('./router/index');
 const session = require('koa-session');
 const passport = require('koa-passport');
 
 const app = new Koa();
-const url = "mongodb://localhost:27017/";
 
 app.keys = ['secret'];
 
@@ -19,23 +16,6 @@ app.use(bodyParser());
 require('./auth');
 app.use(passport.initialize());
 app.use(passport.session());
-
-  /*
-app.use(async (ctx, next) => {
-  console.log('ctx session', ctx.session.user);
-  await next();
-});
-*/
-
-app.use(mongo({
-  host: 'localhost',
-  port: 27017,
-  db: 'ims',
-  authSource: 'admin',
-  max: 100,
-  min: 1,
-  acquireTimeoutMillis: 100
-}));
 
 route(app);
 
